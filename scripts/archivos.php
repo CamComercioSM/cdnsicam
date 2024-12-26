887983ba9bd2ab65c133bdd4ec567ed78c0c4d4f
jpllinasCCSM <jpllinas@ccsm.org.co>
2024-12-26 11:29:20 -0500

    20241216migra

--- scripts/archivos.php
+++ scripts/archivos.php
@@ -150,8 +150,10 @@ function generarJsonEstructuraArbolConIconos($dir, $maxLevel = PHP_INT_MAX, $lev
     $structure = [];
 
     foreach ($files as $file) {
-        $path = $dir . DIRECTORY_SEPARATOR . $file;
         $icon = '';
+        $path = $dir . DIRECTORY_SEPARATOR . $file;
+        $relativePath = str_replace( ('../cdnsicam//'), '', $path); // Obtener la ruta relativa al directorio base        
+        $webPath = ("https://cdnsicam.net/") . str_replace(DIRECTORY_SEPARATOR, '/', ltrim(str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', $relativePath), DIRECTORY_SEPARATOR));
 
         // Determinar el ícono según el tipo de archivo
         if (is_dir($path)) {
@@ -159,36 +161,46 @@ function generarJsonEstructuraArbolConIconos($dir, $maxLevel = PHP_INT_MAX, $lev
             $structure[] = [
                 'text' => $file,
                 'icon' => $icon,
+                'path' => $webPath, // Ruta web completa para copiar
                 'children' => generarJsonEstructuraArbolConIconos($path, $maxLevel, $level + 1)
             ];
         } else {
             if ($level > 0) {
                 // Asignar ícon según extensión de archivo
                 $ext = pathinfo($file, PATHINFO_EXTENSION);
-                switch (strtolower($ext)) {
-                    case 'mp3':
-                        $icon = 'fas fa-music';
-                        break;
-                    case 'txt':
-                        $icon = 'fa fa-file-text';
-                        break;
-                    case 'jpg':
-                    case 'jpeg':
-                    case 'png':
-                    case 'gif':
-                        $icon = 'fa fa-file-image';
-                        break;
-                    case 'pdf':
-                        $icon = 'fa fa-file-pdf';
-                        break;
-                    default:
-                        $icon = 'fa fa-file'; // Ícono por defecto
-                        break;
-                }
+//                switch (strtolower($ext)) {
+//                    case 'mp3':
+//                        $icon = 'fas fa-music';
+//                        break;
+//                    case 'txt':
+//                        $icon = 'fa fa-file-text';
+//                        break;
+//                    case 'jpg':
+//                    case 'jpeg':
+//                    case 'png':
+//                    case 'gif':
+//                        $icon = 'fa fa-file-image';
+//                        break;
+//                    case 'pdf':
+//                        $icon = 'fa fa-file-pdf';
+//                        break;
+//                    default:
+//                        $icon = 'fa fa-file'; // Ícono por defecto
+//                        break;
+//                }
+
+                $icon = match ($ext) {
+                    'mp3' => 'fas fa-music',
+                    'txt' => 'fa fa-file-text',
+                    'jpg', 'jpeg', 'png', 'gif' => 'fa fa-file-image',
+                    'pdf' => 'fa fa-file-pdf',
+                    default => 'fa fa-file', // Ícono predeterminado para archivos
+                };
 
                 $structure[] = [
                     'text' => $file,
                     'icon' => $icon,
+                    'path' => $webPath // Ruta web completa para copiar
                 ];
             }
         }
