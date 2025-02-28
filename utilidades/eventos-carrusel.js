1648ff32933cec3a7192b51098fa95f4913025e0
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2025-02-28 06:55:58 -0500

    Update eventos-carrusel.js

--- utilidades/eventos-carrusel.js
+++ utilidades/eventos-carrusel.js
@@ -1,7 +1,8 @@
-document.addEventListener("DOMContentLoaded", async function () {
+function inyectar_carrusel_eventos(idDiv) {
     try {
         // Obtener el contenido de la página fuente
         const response = await fetch("https://eventosycapacitaciones.tiendasicam32.net/insertar");
+        console.log(response);
         if (!response.ok) throw new Error("Error al obtener el contenido");
         const html = await response.text();
 
@@ -11,11 +12,18 @@ document.addEventListener("DOMContentLoaded", async function () {
         const extractedContent = html || "No se encontró contenido";
 
         // Insertar el contenido en un div específico
-        const container = document.getElementById("contenido-inyectado");
+        const container = document.getElementById(idDiv);
         if (container) {
             container.innerHTML = extractedContent;
         }
     } catch (error) {
         console.error("Error al procesar la solicitud:", error);
     }
-});
+}
+
+inyectar_carrusel_eventos("contenido-inyectado");
+
+setTimeout(() => {  
+    inyectar_carrusel_eventos("contenido-inyectado");
+}, 5000);
+
