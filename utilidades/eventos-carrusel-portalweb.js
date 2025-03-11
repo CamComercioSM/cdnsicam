20ef4d8cbab37f11a296cbe9ed476c1df319291b
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2025-03-11 15:35:48 -0500

    Update eventos-carrusel-portalweb.js

--- utilidades/eventos-carrusel-portalweb.js
+++ utilidades/eventos-carrusel-portalweb.js
@@ -1,23 +1,22 @@
-async function inyectar_carrusel_eventos(idDiv) {
+async function inyectar_carrusel_eventos() {
     try {
         // Obtener el contenido de la página fuente
         const response = await fetch("https://eventosycapacitaciones.tiendasicam32.net/carrusel-portalweb");
         if (!response.ok) throw new Error("Error al obtener el contenido");
         const html = await response.text();
-        // Extraer el contenido o mostrar un mensaje por defecto
-        const extractedContent = html || "No se encontró contenido";
 
-        // Insertar el contenido en un div específico
-        const container = document.getElementById(idDiv);
+        // Seleccionar la sección con la clase 'figura-evento'
+        const container = document.querySelector(".figura-evento");
         if (container) {
-            container.innerHTML = extractedContent;
+            container.innerHTML = html || "No se encontró contenido";
+        } else {
+            console.error("No se encontró la sección '.figura-evento'");
         }
     } catch (error) {
         console.error("Error al procesar la solicitud:", error);
     }
 }
 
-// Llamar a la función 
-setTimeout(() => {
-    inyectar_carrusel_eventos("slide-eventos");
-}, 123);
+// Llamar a la función después de un tiempo de espera
+setTimeout(inyectar_carrusel_eventos, 123);
+
