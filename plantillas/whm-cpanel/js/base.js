188a472daf46b9e65301e5d27dc2fe076c854652
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2025-06-05 09:22:15 -0500

    Update base.js

--- plantillas/whm-cpanel/js/base.js
+++ plantillas/whm-cpanel/js/base.js
@@ -5,26 +5,9 @@ function checkWebsiteStatus(dominio, tiempo_recarga_disponible) {
     const apiChecker = "https://cdnsicam.net/plantillas/whm-cpanel/verificador.php?dominio=" + dominio;
 
     fetch(apiChecker)
-        .then(res => res.json())
-        .then(data => {
-            console.log("Estado del sitio: ", res);
-            console.log("Datos del sitio: ", data);
-            if (data.status === 200 && !data.title.includes("suspended")) {
-                document.getElementById("status-msg").innerHTML = "<h1>Sitio disponible, redirigiendo...</h1>";
-                setTimeout(() => window.location.href = "https://" + dominio, 5000);
-            } else {
-                document.getElementById("status-msg").innerText =
-                    `Sitio responde con estado ${data.status}. Sigue fuera de servicio.`;
-            }
-        })
-        .catch(() => {
-            document.getElementById("status-msg").innerText = "No se pudo contactar al sitio. Sigue fuera de servicio.";
-        });
-
-    fetch(apiChecker)
         .then(response => {
             console.log("Estado del sitio: ", response);
-            console.log("Estado del sitio: ", response.json());
+            console.log("Estado del sitio: ", response.status);
         })
         .catch(() => {
             document.getElementById("status-msg").innerText = "El sitio sigue en mantenimiento. Revisando...";
