ff6904a7246196412ae84c7f32b3668a3ed550bf
Ing. Juan Pablo Llinás Ramírez <jpllinas@ccsm.org.co>
2025-06-05 08:39:03 -0500

    Update base.js

--- plantillas/whm-cpanel/js/base.js
+++ plantillas/whm-cpanel/js/base.js
@@ -2,7 +2,7 @@ function checkWebsiteStatus(url_cuenta, tiempo_recarga_disponible) {
     fetch(url_cuenta)
         .then(response => {
             if (response.status === 200) {
-                document.getElementById("status-msg").innerHTML = "<h1>Sitio disponible, redirigiendo...</h1>";
+                document.getElementById("status-msg").innerHTML = "<h1>Sitio " + url_cuenta + " disponible, redirigiendo...</h1>";
                 setTimeout(() => window.location.href = url_cuenta, tiempo_recarga_disponible * 1000);
             } else {
                 document.getElementById("status-msg").innerText = "El sitio sigue en mantenimiento. Revisando...";
